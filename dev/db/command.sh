cf1c1a28d668b2a50a0e18b5a4035d7e059c964e
Strauss, Christoph (415-Extern) <w11k.strauss@extaccount.com>
2019-08-28 16:43:25 +0200

    kill container if sql script fails

--- dev/db/command.sh
+++ dev/db/command.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # heavily inspired by https://github.com/shanegenschaw/mssql-server-linux
-
-/var/opt/mssql/initdb/initdb.sh & /opt/mssql/bin/sqlservr
+/var/opt/mssql/initdb/initdb.sh &
+/opt/mssql/bin/sqlservr
+exit $?
--- dev/db/initdb.sh
+++ dev/db/initdb.sh
@@ -6,7 +6,15 @@ until /opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD -Q 'SELECT 1;' &>/dev/nu
 done
 
 echo "$0: Initializing database"
+
 for filename in /var/opt/mssql/initdb/*.sql; do
+
   echo "exec $filename"
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i $filename -r
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i $filename -r 0 -b
+  exitCode=$?
+
+  if [ "$exitCode" != 0 ]; then
+    kill -9 $(pgrep sqlservr)
+  fi
+
 done
